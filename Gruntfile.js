7aae8f4786faf831a51218418b29bc02a89fbdb7
Jen Downs <hello@jendowns.com>
2016-11-03 23:33:55 -0500

    Redesigned site: new color scheme, new layout. Also dropped Bootstrap & now using Sass

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+  'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'main.css': 'main.scss'
+                }
+            }
+        },
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              src: ['*.css', '!*.min.css'],
+              ext: '.min.css'
+            }]
+          }
+        },
+        pug: {
+            compile: {
+                options: {
+                    data: {
+                        debug: true
+                    }
+                },
+                files: {
+                    'index.html': ['*.pug']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+     
+    grunt.registerTask('default', ['sass','cssmin','pug']);
+
+};
