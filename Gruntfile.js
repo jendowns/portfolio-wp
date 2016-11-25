614073d10c245be6c0c3389970eb931a98a677d9
Jen Downs <hello@jendowns.com>
2016-11-24 22:51:33 -0600

    Adjusted navbar links, changed font family and styles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,55 @@
 module.exports = function (grunt) {
   'use strict';
 
-    // Project configuration.
-    grunt.initConfig({
+  // Project configuration.
+  grunt.initConfig({
 
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        
-        sass: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'main.css': 'main.scss'
-                }
-            }
-        },
-        cssmin: {
-          target: {
-            files: [{
-              expand: true,
-              src: ['*.css', '!*.min.css'],
-              ext: '.min.css'
-            }]
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'main.css': 'main.scss'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      dist: {
+        files: {
+          'main.min.css': [ 'an.css', 'reset.css', 'icomoon.css', 'main.css']
+        }
+      }
+    },
+    pug: {
+      compile: {
+        options: {
+          data: {
+            debug: true
           }
         },
-        pug: {
-            compile: {
-                options: {
-                    data: {
-                        debug: true
-                    }
-                },
-                files: {
-                    'content-header.php': ['content-header.pug'],
-                    'content-home.php': ['content-home.pug'],
-                    'single.php': ['single.pug']
-                }
-            }
+        files: {
+          'content-header.php': ['content-header.pug'],
+          'content-home.php': ['content-home.pug'],
+          'single.php': ['single.pug'],
+          'header.php': ['header.pug'],
+          'footer.php': ['footer.pug']
         }
-    });
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-pug');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-     
-    grunt.registerTask('default', ['sass','cssmin','pug']);
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+   
+  grunt.registerTask('default', ['sass','cssmin','pug']);
 
 };
---
+++ js/home.js
@@ -0,0 +1,23 @@
+console.log("( ^ o ^ )/");
+
+// ----
+// function debounce(func, wait, immediate) {
+var on_click_square_minimize = debounce(function click_square_minimize(){
+  var elem = $(this).parents(".project-wrapper");
+  $(elem).toggleClass("collapsed");
+  $(this).find(".icon-maximize, .icon-minimize").toggleClass("hidden");
+}, 150, true);
+
+var on_click_square_next_prev = debounce(function click_square_next_prev(){
+  var elem = $(this).parents(".project-wrapper:not(.collapsed)");
+  var img_to_hide = $(elem).find("img:not(.faded-in)");
+  //$(img_to_hide).siblings("img").removeClass("hidden");
+  //$(img_to_hide).addClass("hidden");
+  $(img_to_hide).siblings("img").removeClass("faded-in"); // because there are 2 imgs
+  $(img_to_hide).addClass("faded-in");
+}, 150, true);
+
+$(".square.minimize").on("click", on_click_square_minimize);
+$(".square.prev, .square.next").on("click", on_click_square_next_prev);
+// ---
+
