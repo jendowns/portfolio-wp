20455e2f4f539276d28be58177695490a686648a
Jen Downs <hello@jendowns.com>
2017-01-15 12:49:36 -0600

    Added highlightjs plugin, added more projects w/ writeups, cleaned up main.scss & footer

--- functions.php
+++ functions.php
@@ -8,8 +8,20 @@ function enqueue_styles() {
     wp_register_style( 'style', THEME_DIR . '/style.css' );
     wp_enqueue_style( 'style' );  
 
-	wp_register_style( 'lato-font', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900' );
-	wp_enqueue_style( 'lato-font' );  
+  	wp_register_style( 'lato-font', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900' );
+  	wp_enqueue_style( 'lato-font' );
+
+    if(is_single()){
+      wp_register_style( 'code-highlight-style', THEME_DIR . '/css/paraiso-dark.css' );
+        wp_enqueue_style( 'code-highlight-style' ); 
+    }  
+}
+
+function enqueue_highlighter_script() {
+  if(is_single()){
+    wp_register_script('code-highlight-script', THEME_DIR . '/js/highlight.pack.js');
+      wp_enqueue_script('code-highlight-script');
+  }
 }
 
 function register_jquery() {
@@ -21,6 +33,8 @@ function register_jquery() {
     wp_deregister_script('jquery-migrate');
   }
 }
+
+add_action( 'wp_enqueue_scripts', 'enqueue_highlighter_script' );
 
 add_action( 'wp_enqueue_scripts', 'register_jquery' );
 
